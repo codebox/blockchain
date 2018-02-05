0005de80d6b9ba5fac690ae9a3cd94d4ca580577
rob <rob@codebox.org.uk>
2018-02-05 12:49:07 +0000

    client sending transactions

--- src/blockchain/client/commands/send.py
+++ src/blockchain/client/commands/send.py
@@ -1,16 +1,53 @@
 from blockchain.common.transaction import Transaction
 from blockchain.common.network import Network
+from blockchain.common.blockchain import Blockchain
+import re
+
+ADDRESS_PATTERN = re.compile('^[a-f0-9]{64}$')
 
 class SendCommand:
     NAME  = 'send'
-    USAGE = '{} <amount> <address>'.format(NAME)
+    USAGE = '{} <from address> <amount> <to address>'.format(NAME)
 
     def __init__(self, *args):
         if len(args) != 3:
             print('wrong number of args for {}'.format(SendCommand.NAME))
 
         else:
-            from_address, amount, to_address = args
-            t = Transaction(from_address, amount, to_address)
-            net = Network()
-            net.send_transaction(t)
+            from_address, amount_txt, to_address = args
+
+            if not self._is_valid_address_format(from_address):
+                print('invalid from address')
+
+            elif not self._is_valid_address_format(to_address):
+                print('invalid to address')
+
+            elif not self._is_valid_amount_format(amount_txt):
+                print('invalid amount')
+
+            else:
+                blockchain = Blockchain()
+                balance = blockchain.get_balance_for_address(from_address)
+                amount = float(amount_txt)
+
+                if balance is None:
+                    print('client does not own specified From Address')
+
+                elif balance < amount:
+                    print('insufficient funds')
+
+                else:
+                    t = Transaction(from_address, amount, to_address)
+                    net = Network()
+                    net.send_transaction(t)
+
+    def _is_valid_address_format(self, address_candidate):
+        return ADDRESS_PATTERN.match(address_candidate)
+
+    def _is_valid_amount_format(self, amount_txt):
+        try:
+            amount = float(amount_txt)
+            return amount > 0
+        except ValueError:
+            return False
+
---
+++ src/blockchain/common/blockchain.py
@@ -0,0 +1,14 @@
+DATA_FILE = 'blocks'
+
+class Blockchain:
+    def __init__(self):
+        pass
+
+    def load_from_file(self, file_path):
+        pass
+
+    def load_from_network(self):
+        pass
+
+    def get_balance_for_address(self, address):
+        return 100 # TODO
